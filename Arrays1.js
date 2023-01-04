37c79723129babdb02d7248700eb5c9d0262b45c
Oussema618 <121972843+Oussema618@users.noreply.github.com>
2023-01-04 21:02:20 +0100

    Update Arrays1.js

--- Arrays1.js
+++ Arrays1.js
@@ -1,1 +1,50 @@
-
+Basic Requirments:
+1/ var myArray = [1,2,3,4,5]
+2/ a. 0
+  b. 2
+  c. 3
+3/ [1, 2 3 4 5, 6, 7, "The", "quick", "brown" , "fox" ,"jumped", "over" ," the lazy", "dog", true, false, true ]
+4/ function emptyArray() {
+   var eArray = []
+  return eArray;
+}  
+5/ function numbersArray() {
+   var nArray = [1,2,3,4,5]
+  return nArray; 
+}  
+6/ function booleansArray() {
+   var bArray = [true, true, true]
+  return bArray; 
+}  
+7/ function booleansArray() {
+   var sArray = ["Ahmed", "Vega"]
+  return sArray;
+8/ function arrayLength(array) {
+   return array.length
+} 
+More Practice 
+1/ function firstElement(array) {
+   return array[0];
+}  
+2/ function lastElement(array) {
+   return array[length-1];
+}  
+3/ function getNthElement(array, number) {
+   return array[number]; 
+}   
+4/ function push(array, element) {
+    var toadd = element;
+   array.push(toadd);
+    return array;
+}  
+5/ function pop(array) {
+   return array.shift(); 
+}  
+6/ function unshift(array, element) {
+   return array.unshift(element);
+}  
+7/ function shift(array) {
+  return array.pop();
+}  
+8/
+  
